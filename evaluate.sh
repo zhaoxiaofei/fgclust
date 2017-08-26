a9d83d9cce0017a192f0b109fd5b8678c8589cd8
XiaoFei Zhao <xzhao@fusiongenomics.com>
2017-08-26 02:31:12 -0700

    Modified evaluate.sh to timeout at 50 hours and to redirect stdout and stderr of the timed program into the log files with the extesnion time

--- evaluate.sh
+++ evaluate.sh
@@ -24,18 +24,18 @@ function resetfile() {
 }
 
 function run_mine_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_mine_with_infastafile_seqid($1, $2, $3, $4) began at $(date)"
         if [ $4 -lt 20 ]; then flag="--alphasize_sim $4"; else flag=""; fi
         date; cat "$1.faa"            | "${ROOTDIR}"/bin/fastaseqs-to-distmatrix.out --edsim $3 $flag > "$2-$3.distmatrix"
         date; cat "$2-$3.distmatrix"  | "${ROOTDIR}"/bin/linsetcover.out                              > "$2-$3.ordsetcover"
         date; cat "$2-$3.ordsetcover" | "${ROOTDIR}"/bin/setcover-ords-to-hdrs.out $1.faa             > "$2-$3.hdrsetcover-clu.tsv"
         echo "run_mine_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.mine.time"
+    } } 2>&1 | tee "$2-$3.mine.time"
 }
 
 function run_quaclust_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_mmseqsclust_with_infastafile_seqid($1, $2, $3) began at $(date)"
         rm -r "$2-$3.quaclust-tmpdir" || true
         mkdir "$2-$3.quaclust-tmpdir"
@@ -47,11 +47,11 @@ function run_quaclust_with_infastafile_seqid() {
         if [ "${ISTIMEOUT}" == true ] ; then quaclust_timeout=true; return 0 ; fi
         date; "${ROOTDIR}"/benchmark/bin/mmseqs createtsv             "$1.mmseqs-db" "$1.mmseqs-db"       "$2-$3.quaclust-clu" "$2-$3.quaclust-clu.tsv"
         echo "run_mmseqsclust_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.quaclust.time"
+    } } 2>&1 | tee "$2-$3.quaclust.time"
 }
 
 function run_linclust_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_linclust_with_infastafile_seqid($1, $2, $3) began at $(date)"
         rm -r "$2-$3.linclust-tmpdir" || true
         mkdir "$2-$3.linclust-tmpdir"
@@ -60,11 +60,11 @@ function run_linclust_with_infastafile_seqid() {
         date; "${ROOTDIR}"/benchmark/bin/mmseqs linclust         "$1.mmseqs-db" "$2-$3.linclust-clu" "$2-$3.linclust-tmpdir" --min-seq-id 0.$3
         date; "${ROOTDIR}"/benchmark/bin/mmseqs createtsv        "$1.mmseqs-db" "$1.mmseqs-db" "$2-$3.linclust-clu" "$2-$3.linclust-clu.tsv"
         echo "run_linclust_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.linclust.time"
+    } } 2>&1 | tee "$2-$3.linclust.time"
 }
 
 function run_kclust_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_kclust_with_infastafile_seqid($1, $2, $3) began at $(date)"
         set +e
         date; timeout $4 "${ROOTDIR}"/benchmark/bin/kClust -i "$1.faa" -d "$2-$3.kclust" -s $(python -c "print(round(-0.68506329113924050632 + 0.06025316455696202531*$3, 2))")
@@ -72,7 +72,7 @@ function run_kclust_with_infastafile_seqid() {
         if [ "${ISTIMEOUT}" == true ] ; then kclust_timeout=true; return 0 ; fi
         date; cat "$2-$3.kclust/clusters.dmp" | tail -n +2 | awk '{print $2"\t"$1"\t"1}' | "${ROOTDIR}"/bin/setcover-ords-to-hdrs.out "$1.faa" > "$2-$3.kclust-clu.tsv"
         echo "run_kclust_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.kclust.time"
+    } } 2>&1 | tee "$2-$3.kclust.time"
 }
 
 function run_cdhit_with_infastafile_seqid() {
@@ -83,7 +83,7 @@ function run_cdhit_with_infastafile_seqid() {
     cdhitwordsize=5
     if [ $SIM -lt 70 ] ; then cdhitwordsize=4; fi
     if [ $SIM -lt 60 ] ; then cdhitwordsize=3; fi
-    time -p {
+    { time -p {
         echo "run_cdhit_with_infastafile_seqid($1, $2, $3) began at $(date)"
         set +e
         date; timeout $4 "${ROOTDIR}"/benchmark/bin/cd-hit -i "$1.faa" -o "$2_cdhit-M0-T0-d0-s80-c$3-n$cdhitwordsize.faa" -M 0 -T 0 -d 0 -s 0.8 -c 0.$3 -n $cdhitwordsize
@@ -91,26 +91,26 @@ function run_cdhit_with_infastafile_seqid() {
         if [ "${ISTIMEOUT}" == true ] ; then cdhit_timeout=true; return 0 ; fi
         date; cat "$2_cdhit-M0-T0-d0-s80-c$3-n$cdhitwordsize.faa.clstr" | "${ROOTDIR}"/benchmark/src/clstr-to-clu-tsv.py > "$2-$3.cdhit-clu.tsv"
         echo "run_cdhit_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.cdhit.time" 
+    } } 2>&1 | tee "$2-$3.cdhit.time" 
 }
 
 function run_minenuc_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_minenuc_with_infastafile_seqid($1, $2, $3) began at $(date)"
         date; cat "$1.fna"            | "${ROOTDIR}"/bin/fastaseqs-to-distmatrix.out --edsim $3 --isnuc 1 > "$2-$3.distmatrix"
         date; cat "$2-$3.distmatrix"  | "${ROOTDIR}"/bin/linsetcover.out                        > "$2-$3.ordsetcover"
         date; cat "$2-$3.ordsetcover" | "${ROOTDIR}"/bin/setcover-ords-to-hdrs.out $1.fna       > "$2-$3.hdrsetcover-clu.tsv"
         echo "run_minenuc_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.minenuc.time"
+    } } 2>&1 | tee "$2-$3.minenuc.time"
 }
 
 function run_vsearch_with_infastafile_seqid() {
-    time -p {
+    { time -p {
         echo "run_vsearch_with_infastafile_seqid($1, $2, $3) began at $(date)"
         date; "${ROOTDIR}"/benchmark/bin/vsearch --cluster_smallmem $1.fna --id 0.$3 -uc "$2_vsearch-$3.uc"
         date; cat "$2_vsearch-$3.uc" | "${ROOTDIR}"/benchmark/src/uc-to-clu-tsv.py > "$2_vsearch-$3-clu.tsv"
         echo "run_vsearch_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.vsearch.time"
+    } } 2>&1 | tee "$2-$3.vsearch.time"
 }
 
 function run_cdhitest_with_infastafile_seqid() {
@@ -119,12 +119,12 @@ function run_cdhitest_with_infastafile_seqid() {
         return 0
     fi
     cdhitestwordsize=10
-    time -p {
+    { time -p {
         echo "run_cdhitest_with_infastafile_seqid($1, $2, $3) began at $(date)"
         date; "${ROOTDIR}"/benchmark/bin/cd-hit-est -i "$1.fna" -o "$2_cdhitest-M0-T0-d0-s80-c$3-n$cdhitestwordsize.fna" -M 0 -T 0 -d 0 -s 0.8 -c 0.$3 -n $cdhitestwordsize
         date; cat "$2_cdhitest-M0-T0-d0-s80-c$3-n$cdhitestwordsize.fna.clstr" | "${ROOTDIR}"/benchmark/src/clstr-to-clu-tsv.py > "$2-$3.cdhitest-clu.tsv"
         echo "run_cdhitest_with_infastafile_seqid($1, $2, $3) ended at $(date)"
-    } | tee "$2-$3.cdhitest.time"
+    } } 2>&1 | tee "$2-$3.cdhitest.time"
 }
 
 for SIM in $(echo $CSVSIM | sed "s/,/ /g"); do
@@ -160,15 +160,13 @@ for SIM in $(echo $CSVSIM | sed "s/,/ /g"); do
     # skip the rest if sim is either 60 or 80 
     if [ "60,80" == *"$SIM"* ]; then continue; fi
 
-    ## run pdb
-    
-    if [[ $4 == *"mine"* ]];     then run_mine_with_infastafile_seqid     "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
-    if [[ $4 == *"linclust"* ]]; then run_linclust_with_infastafile_seqid "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
-    if [[ $4 == *"quaclust"* ]]; then run_quaclust_with_infastafile_seqid "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
-    if [[ $4 == *"cdhit"* ]];    then run_cdhit_with_infastafile_seqid    "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
-    if [[ $4 == *"kclust"* ]];   then run_kclust_with_infastafile_seqid   "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
-    
+    ## run pdb 
     (
+        if [[ $4 == *"mine"* ]];     then run_mine_with_infastafile_seqid     "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
+        if [[ $4 == *"linclust"* ]]; then run_linclust_with_infastafile_seqid "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
+        if [[ $4 == *"quaclust"* ]]; then run_quaclust_with_infastafile_seqid "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
+        if [[ $4 == *"cdhit"* ]];    then run_cdhit_with_infastafile_seqid    "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
+        if [[ $4 == *"kclust"* ]];   then run_kclust_with_infastafile_seqid   "${INPREF}/pdbent-seqres_shuf" "${OUTDIR}/pdbent-seqres_shuf" $SIM 3334; fi
         flock -e 200
         cat "${OUTDIR}/pdbent-seqres_shuf-${SIM}."*clu.tsv | "${ROOTDIR}"/benchmark/src/update_memtable.py "${OUTDIR}/pdbent-seqres_shuf_memtable.tsv"
         function clu_tsv_to_tms() {
@@ -186,7 +184,7 @@ for SIM in $(echo $CSVSIM | sed "s/,/ /g"); do
     
     ## run uniprot
 
-    timelimit=20000
+    timelimit=$((3600*50))
     cdhit_timeout=false
     quaclust_timeout=false
     kclust_timeout=false
@@ -198,7 +196,7 @@ for SIM in $(echo $CSVSIM | sed "s/,/ /g"); do
         if [[ $4 == *"quaclust"* ]]; then run_quaclust_with_infastafile_seqid "${INPREF}/${uniref}_shuf" "${OUTDIR}/${uniref}_shuf" $SIM $timelimit; fi
         if [[ $4 == *"cdhit"* ]];    then run_cdhit_with_infastafile_seqid    "${INPREF}/${uniref}_shuf" "${OUTDIR}/${uniref}_shuf" $SIM $timelimit; fi
         if [[ $4 == *"kclust"* ]];   then run_kclust_with_infastafile_seqid   "${INPREF}/${uniref}_shuf" "${OUTDIR}/${uniref}_shuf" $SIM $timelimit; fi
-        timelimit=$(($timelimit*2))
+        #timelimit=$(($timelimit*2))
     done
 done
 
