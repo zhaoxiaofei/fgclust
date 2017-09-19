af02aa71cdc156a7cfee6ad3e3968ef8fa6bafad
XiaoFei Zhao <xzhao@fusiongenomics.com>
2017-09-19 13:31:41 -0700

    Fixed calculation of specificity on the Pfam dataset.

--- benchmark/src/gen-pdbent-cdf.py
+++ benchmark/src/gen-pdbent-cdf.py
@@ -5,27 +5,21 @@ import sys
 
 def process_tmsfname(tmsfname):
     ret = [0] * 10
+    nclusters = 0
     with open(tmsfname) as tmsfile:
         for line in tmsfile:
             line = line.strip()
             if line.startswith('tmscore'):
-                tms10 = math.ceil(float(line.split()[1]) * 10) - 1
-                assert 0 <= tms10 and tms10 <= 9, 'tms10={}'.format(tms10)
-                ret[int(tms10)] += 1
+                nclusters += 1
+                for tok in line.split()[4:]:
+                    tms10 = math.ceil(float(tok.lstrip('[').rstrip(']').rstrip(',')) * 10) - 1
+                    assert 0 <= tms10 and tms10 <= 9, 'tms10={}'.format(tms10)
+                    ret[int(tms10)] += 1
     for i in range(1, 10, 1):
         ret[i] = ret[i-1] + ret[i]
-    return ret
+    return (nclusters, ret)
 
-mine     = process_tmsfname(sys.argv[1])
-if len(sys.argv) > 2:
-    linclust = process_tmsfname(sys.argv[2])
-    cdhit    = process_tmsfname(sys.argv[3])
-    quaclust = process_tmsfname(sys.argv[4])
-    kclust   = process_tmsfname(sys.argv[5])
-
-#print('TM score & mine & linclust &cd-hit')
-#for i in range(10):
-#    print('{} & {} & {} & {}'.format(round((i+1)/10, 1), mine[i], linclust[i], cdhit[i]))
+nclu, mine     = process_tmsfname(sys.argv[1])
 
 sim = 0
 if '-50.' in sys.argv[1]:
@@ -38,22 +32,17 @@ assert sim > 0
 
 progname = None
 if 'hdrsetcover' in sys.argv[1]:
-    progname = 'FgClust'
+    progname = 'FgClust  '
 if 'linclust' in sys.argv[1]:
-    progname = 'Linclust'
+    progname = 'Linclust '
 if 'quaclust' in sys.argv[1]:
-    progname = 'MMSeqs2'
+    progname = 'MMSeqs2  '
 if 'cdhit' in sys.argv[1]:
-    progname = 'CD-HIT'
+    progname = 'CD-HIT   '
 if 'kclust' in sys.argv[1]:
-    progname = 'kClust'
+    progname = 'kClust   '
 assert progname
 
-print('TM score  & {} \\\\'.format(' & '.join([str((i+1)/10.0)  for i in range(1, 10)])))
-print('{} & {} & {} \\\\'.format(progname, sim, ' & '.join([str(mine[i])     for i in range(1, 10)])))
-if len(sys.argv) > 2:
-    print('Linclust  & {} & {} \\\\'.format(sim, ' & '.join([str(linclust[i]) for i in range(1, 10)])))
-    print('CD-HIT    & {} & {} \\\\'.format(sim, ' & '.join([str(cdhit[i])    for i in range(1, 10)])))
-    print('MMseqs2   & {} & {} \\\\'.format(sim, ' & '.join([str(quaclust[i]) for i in range(1, 10)])))
-    print('kClust    & {} & {} \\\\'.format(sim, ' & '.join([str(kclust[i])   for i in range(1, 10)])))
+print('TM-score & sim & nclusters & {} \\\\'.format(' & '.join([str((i+1)/10.0)  for i in range(1, 9)])))
+print('{} & {} & {} & {} \\\\'.format(progname, sim, nclu, ' & '.join([str(mine[i])     for i in range(1, 9)])))
 
